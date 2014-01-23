5667619833dc8de1150d749da58db4ba288d2895
Shane Gough <shane@thegaragelab.com>
2014-01-23 17:17:23 +1000

    Added a simple set of templates for creating projects.

---
+++ library/template/lpc1114/README.md
@@ -0,0 +1,4 @@
+# Template for LPC1114 Microboard projects
+
+This directory contains the minimum files required for an LPC1114 Microboard
+project.
---
+++ library/template/main.c
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------------*
+* Main program template.
+*---------------------------------------------------------------------------*/
+#include <microboard.h>
+
+/** Initialisation
+ *
+ * Use this function to do any one time initialisation. The main Microboard
+ * will already have been called so you only need to initialise the slots and
+ * features you intend to use in your program.
+ */
+static void init() {
+  // TODO: Initialise any required slots
+  // TODO: Set up any peripherials used
+  }
+
+/** Main program loop
+ *
+ * This function is called for every iteration of the main program loop.
+ */
+static void loop() {
+  // TODO: Implement this
+  }
+
+/** Program entry point
+ *
+ * Initialise the Microboard and any needed peripherals and then go into the
+ * main processing loop.
+ */
+void main() {
+  mbInit();
+  // Program specific initialisation
+  init();
+  // Main loop
+  while(1)
+    loop();
+  }
