24de829e30bd076d7bb557081d0528ca8842f89e
shane <shane@thegaragelab.com>
2014-01-23 22:09:37 +1000

    Added an empty 'mbInit()' function just so the linker is satisfied.

---
+++ library/src/lpc1114/mbinit.c
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------*
+* Board initialisation and common data
+*----------------------------------------------------------------------------*
+* 23-Jan-2014 ShaneG
+*
+*  Implements the 'mbInit()' function as well as defining the static arrays
+*  needed throughout the library.
+*--------------------------------------------------------------------------*/
+#include <microboard.h>
+
+/** Board level initialisation
+ *
+ * This function does any system wide initialisation that is required. This
+ * must be provided by all implementations but it may not do anything. It
+ * must be the first function called by the user application.
+ */
+void mbInit(void) {
+  // TODO: Implement me
+  }
+
