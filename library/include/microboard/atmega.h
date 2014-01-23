c88c04c541dc7d94880cbf66fae8e69f6e582052
Shane Gough <shane@thegaragelab.com>
2014-01-23 10:07:00 +1000

    Added template includes for ATmega and PIC16F targets.

---
+++ library/include/microboard/atmega.h
@@ -0,0 +1,44 @@
+/*--------------------------------------------------------------------------*
+* Microboard Interface Library
+* Copyright (c) 2014, The Garage Lab (http://thegaragelab.com)
+*---------------------------------------------------------------------------*
+* 23-Jan-2014 ShaneG
+*
+* Initial version. Developed for the ATmega Microboard.
+*--------------------------------------------------------------------------*/
+#ifndef __MICROBOARD_ATMEGA_H
+#define __MICROBOARD_ATMEGA_H
+
+/* Check inclusion order */
+#ifndef __MICROBOARD_H
+#  error "Do not include this file directly. Include <microboard.h> instead."
+#endif
+
+//---------------------------------------------------------------------------
+// Configure optional implementation details
+//---------------------------------------------------------------------------
+
+// If defined then analog operations are supported by the board
+//#define MB_OPT_ANALOG
+
+// If defined then I2C operations are supported by the board
+//#define MB_OPT_I2C
+
+// If defined then serial operations are supported by the board
+//#define MB_OPT_SERIAL
+
+//---------------------------------------------------------------------------
+// Analog configuration
+//---------------------------------------------------------------------------
+
+// The maximum value returned by 'mbAnalogGet()' (in this case 10 bits)
+#define MB_ANALOG_MAX_GET ((1<<10)-1)
+
+// The maximum value accepted by 'mbAnalogSet()' (in this case 10 bits)
+#define MB_ANALOG_MAX_SET ((1<<10)-1)
+
+// If defined then analog values can be set with the function 'mbAnalogSet()'.
+//#define MB_ANALOG_SET
+
+#endif /* __MICROBOARD_ATMEGA_H */
+
