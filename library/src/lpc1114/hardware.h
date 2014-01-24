6612ebbb8e4d7e2d03ba6b23aeab85920138202d
Shane Gough <shane@thegaragelab.com>
2014-01-24 17:02:29 +1000

    Added pointer to the IOCON register as part of the pin mapping info.

--- library/src/lpc1114/hardware.h
+++ library/src/lpc1114/hardware.h
@@ -12,12 +12,17 @@
 #pragma pack(push, 1)
 
 /** Defines a single pin on a slot
+ *
+ * This maps a Clixx slot and pin to one of the GPIO pins on the LPC1114.
+ * For each pin we store the GPIO port, the pin number on that port and a
+ * pointer to the IO configuration register for the pin.
  */
 typedef struct _PINDEF {
-  uint8_t m_port;     //! The GPIO port (0 - 3)
-  uint8_t m_pin;      //! The GPIO pin (0 - 31)
-  uint8_t m_used;     //! If 0 this pin is not used
-  uint8_t m_reserved; //! Space this structure to a 32 bit value
+  uint8_t             m_port;     //! The GPIO port (0 - 3)
+  uint8_t             m_pin;      //! The GPIO pin (0 - 31)
+  uint8_t             m_used;     //! If 0 this pin is not used
+  uint8_t             m_reserved; //! Space this structure to a 32 bit value
+  volatile uint32_t *m_iocon;    //! IO control register for this pin
   } PINDEF;
 
 // Restore previous packing
@@ -26,7 +31,8 @@ typedef struct _PINDEF {
 /** Pin definition table.
  *
  * This table contains the definitions for each of the four possible user
- * configurable pins on each of the 6 slots (24 entries).
+ * configurable pins on each of the 6 slots (24 entries). The table will be
+ * stored in flash so it won't use any RAM.
  */
 extern const PINDEF g_pindefs[];
 
